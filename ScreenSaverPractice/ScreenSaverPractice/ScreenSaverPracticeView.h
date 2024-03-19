8eeb6e88459481c3f5f3ff61fc139a24957ab792
Sarah Clark <sarahireneclark@gmail.com>
2024-03-19 12:54:48 -0500

    Initial commit of project

---
+++ ScreenSaverPractice/ScreenSaverPractice/ScreenSaverPracticeView.h
@@ -0,0 +1,12 @@
+//
+//  ScreenSaverPracticeView.h
+//  ScreenSaverPractice
+//
+//  Created by Sarah Clark on 3/19/24.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface ScreenSaverPracticeView : ScreenSaverView
+
+@end
---
+++ ScreenSaverPractice/ScreenSaverPractice/ScreenSaverPracticeView.m
@@ -0,0 +1,51 @@
+//
+//  ScreenSaverPracticeView.m
+//  ScreenSaverPractice
+//
+//  Created by Sarah Clark on 3/19/24.
+//
+
+#import "ScreenSaverPracticeView.h"
+
+@implementation ScreenSaverPracticeView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
